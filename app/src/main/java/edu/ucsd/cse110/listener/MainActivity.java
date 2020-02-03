fcf6a84541c378879669e382f4ca4d873df3e3dd
Sander Valstar <sander@email.com>
2020-02-03 13:27:00 -0800

    migrate to android x

--- app/src/main/java/edu/ucsd/cse110/listener/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/listener/MainActivity.java
@@ -1,7 +1,7 @@
 package edu.ucsd.cse110.listener;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import java.util.Observable;
